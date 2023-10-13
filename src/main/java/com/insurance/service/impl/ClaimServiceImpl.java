e7c85181226ac62d4fca34dc054dfe3362cdcc6d
AakashBawale <aakashbawale@gmail.com>
2023-10-13 14:18:23 +0530

    added get all claims

--- src/main/java/com/insurance/service/impl/ClaimServiceImpl.java
+++ src/main/java/com/insurance/service/impl/ClaimServiceImpl.java
@@ -1,5 +1,6 @@
 package com.insurance.service.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,24 +12,32 @@ import com.insurance.repository.ClaimRepository;
 import com.insurance.service.ClaimService;
 
 @Service
-public class ClaimServiceImpl implements ClaimService{
+public class ClaimServiceImpl implements ClaimService {
 //inject repository
 	@Autowired
 	private ClaimRepository claimRepository;
+
 	@Override
 	public Claim saveClaim(Claim claim) {
-		Claim claim1=claimRepository.save(claim);
+		Claim claim1 = claimRepository.save(claim);
 		return claim1;
 	}
+
 	@Override
 	public void deleteClaimById(Integer id) {
-	     Optional<Claim> deleteClaim = claimRepository.findById(id);
-	     
-	     if (deleteClaim.isPresent()) {
-				claimRepository.delete(deleteClaim.get());
-			} else {
-				throw new ResourceNotFoundException("No Claim found with id: " + id);
-		
+		Optional<Claim> deleteClaim = claimRepository.findById(id);
+
+		if (deleteClaim.isPresent()) {
+			claimRepository.delete(deleteClaim.get());
+		} else {
+			throw new ResourceNotFoundException("No Claim found with id: " + id);
+
+		}
 	}
+
+	@Override
+	public List<Claim> getAllClaimsList() {
+		List<Claim> claimList = claimRepository.findAll();
+		return claimList;
 	}
 }
