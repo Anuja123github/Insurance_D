fc2f8dd0e6c9724e88de5915b713717008105bc9
anand <anand.bhunia18@gmail.com>
2023-10-14 13:40:49 +0530

    update policy in progress

--- src/main/java/com/insurance/controller/ClaimController.java
+++ src/main/java/com/insurance/controller/ClaimController.java
@@ -1,7 +1,6 @@
 package com.insurance.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,6 +52,13 @@ public class ClaimController {
 		// CR-772
 		List<Claim> claimList = claimService.getAllClaimsList();
 		return ResponseEntity.status(HttpStatus.OK).body(claimList);
+	}
+	
+	@PutMapping("/updatePolicyClaim")
+	public ResponseEntity<Policy> updatePolicyClaims(@RequestBody Policy policy) {
+		
+		
+		return null;
 	}
 
 }
